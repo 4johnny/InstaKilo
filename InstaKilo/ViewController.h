8ab480fabcccdee17a270ff3f3766006db229dbd
4johnny <4johnny@users.noreply.github.com>
2015-01-29 10:30:53 -0800

    Initial Commit

---
+++ InstaKilo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InstaKilo
+//
+//  Created by Johnny on 2015-01-29.
+//  Copyright (c) 2015 Empath Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
